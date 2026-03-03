089bf2f9c56a4325c565bc319e6af8332b5e3672
Psharma6141 <rashika8025@gmail.com>
2026-03-04 01:56:33 +0530

    Update subscription.controller.js

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -9,16 +9,16 @@ export const createSubscription = async (req, res, next) =>{
         user: req.user._id,
      });
 
-    //  const { workflowRunId } = await workflowClient.trigger({
-    //   url: `${SERVER_URL}/api/v1/workflows/subs/reminder`,
-    //   body: {
-    //     subscriptionId: subscription._id,
-    //   },
-    //   headers: {
-    //     "content-type": "application/json",
-    //   },
-    //   retries: 0,
-    // });
+     const { workflowRunId } = await workflowClient.trigger({
+      url: `${SERVER_URL}/api/v1/workflows/subs/reminder`,
+      body: {
+        subscriptionId: subscription._id,
+      },
+      headers: {
+        "content-type": "application/json",
+      },
+      retries: 0,
+    });
 
     //  res.status(201).json({success: true, data: {subscription, workflowRunId} });
      res.status(201).json({success: true, data: {subscription} });
