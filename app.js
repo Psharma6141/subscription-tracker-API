fb5bd912eb4e31826f74c28bf27e1ce6af5a492d
Psharma6141 <rashika8025@gmail.com>
2026-03-04 14:36:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.get('/', function (req, res){
 
 console.log("jii");
 
-
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=>{
     console.log(`server running on http://localhost:${PORT}`);
 })
